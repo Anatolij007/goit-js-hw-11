b95fe14f736eabadf996a9f922a7cba9d410de97
Anatolij007 <98270281+Anatolij007@users.noreply.github.com>
2022-09-02 18:12:49 +0300

    бібліотеки

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { renderGallery } from '/src/render';
---
+++ src/render.js
@@ -0,0 +1,28 @@
+export function renderGallery() {
+  const markup = {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  };
+  return `<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+}
