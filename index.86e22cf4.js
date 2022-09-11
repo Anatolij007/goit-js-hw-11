3629b12a30ffbf94f29ae61161265ef27bbcc652
Anatolij007 <98270281+Anatolij007@users.noreply.github.com>
2022-09-11 23:51:23 +0000

    Deploying to gh-pages from @ Anatolij007/goit-js-hw-11@62d00be517700d894e96ed71f8ec546df2c28b4e 🚀

--- index.86e22cf4.js
+++
@@ -1,2 +0,0 @@
-const e={searchForm:document.querySelector("#search-form"),gallery:document.querySelector(".gallery"),loadMore:document.querySelector(".load-more")};console.log(e.searchForm),e.searchForm.addEventListener("sumbit",(function(e){e.preventDefault();fetch("https://newsapi.org/v2/everything?q=tesla&pageSize=5&page=1",{headers:{"X-Api-Key":"2087ad7622f241ad8284941576a45d1b"}}).then((e=>e.json())).catch(console.log())})),e.loadMore.addEventListener("click",(function(){}));
-//# sourceMappingURL=index.86e22cf4.js.map
